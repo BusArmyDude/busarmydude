61e1f8118ea3bccd9b108e6f909dbaedb40774a3
BusArmyDude <busarmydude@gmail.com>
2024-12-09 17:45:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,22 +19,22 @@ function toggleDarkMode() {
 // Twitch Follower Count Function
 async function fetchTwitchFollowerCount() {
     const clientId = 'rffm694jo6hbtx6imyvpz5phn890nt';
-    const accessToken = 'pmo4j1t6of8cq82geeu6ywo1qxmmvp';
+    const accessToken = 'v1usw7dyvwwzg9sdc6za4x4kjpt26g';
     const twitchUsername = 'busarmydude';
 
-    const userResponse = await fetch(`https://api.twitch.tv/helix/users?login=${twitchUsername}`, {
+    const userResponse = await fetch(`https://api.twitch.tv/helix/users?login=${busarmydude}`, {
         headers: {
-            'Client-ID': clientId,
-            'Authorization': `Bearer ${accessToken}`
+            'Client-ID': rffm694jo6hbtx6imyvpz5phn890nt,
+            'Authorization': `Bearer ${v1usw7dyvwwzg9sdc6za4x4kjpt26g}`
         }
     });
     const userData = await userResponse.json();
     const userId = userData.data[0].id;
 
-    const followerResponse = await fetch(`https://api.twitch.tv/helix/users/follows?to_id=${userId}`, {
+    const followerResponse = await fetch(`https://api.twitch.tv/helix/users/follows?to_id=${busarmydude}`, {
         headers: {
-            'Client-ID': clientId,
-            'Authorization': `Bearer ${accessToken}`
+            'Client-ID': rffm694jo6hbtx6imyvpz5phn890nt,
+            'Authorization': `Bearer ${v1usw7dyvwwzg9sdc6za4x4kjpt26g}`
         }
     });
     const followerData = await followerResponse.json();
