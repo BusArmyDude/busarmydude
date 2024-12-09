14b4a68f873e8c031d9c7e4e411d622422f8faa2
BusArmyDude <busarmydude@gmail.com>
2024-12-09 17:47:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,36 +15,3 @@ function toggleDarkMode() {
         localStorage.setItem("dark-mode", "disabled");
     }
 }
-
-// Twitch Follower Count Function
-async function fetchTwitchFollowerCount() {
-    const clientId = 'rffm694jo6hbtx6imyvpz5phn890nt';
-    const accessToken = 'v1usw7dyvwwzg9sdc6za4x4kjpt26g';
-    const twitchUsername = 'busarmydude';
-
-    const userResponse = await fetch(`https://api.twitch.tv/helix/users?login=${busarmydude}`, {
-        headers: {
-            'Client-ID': rffm694jo6hbtx6imyvpz5phn890nt,
-            'Authorization': `Bearer ${v1usw7dyvwwzg9sdc6za4x4kjpt26g}`
-        }
-    });
-    const userData = await userResponse.json();
-    const userId = userData.data[0].id;
-
-    const followerResponse = await fetch(`https://api.twitch.tv/helix/users/follows?to_id=${busarmydude}`, {
-        headers: {
-            'Client-ID': rffm694jo6hbtx6imyvpz5phn890nt,
-            'Authorization': `Bearer ${v1usw7dyvwwzg9sdc6za4x4kjpt26g}`
-        }
-    });
-    const followerData = await followerResponse.json();
-    const followerCount = followerData.total;
-
-    // Display follower count on the page
-    document.getElementById('twitch-follower-count').innerText = followerCount;
-}
-
-// Fetch follower count on page load
-window.onload = function() {
-    fetchTwitchFollowerCount();
-};
